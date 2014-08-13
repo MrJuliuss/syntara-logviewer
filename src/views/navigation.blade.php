e7eaffd6f2755fa2e7698ccd40e7f29b01f58e6c
Mochamad Gufron <mgufrone@users.noreply.github.com>
2014-08-13 09:58:56 +0700

    Change URL log viewer on navigation

--- src/views/navigation.blade.php
+++ src/views/navigation.blade.php
@@ -1,5 +1,5 @@
 <li class="">
-    <a href="{{ Config::get('syntara::config.uri') }}/logviewer">
+    <a href="{{ URL::to(Config::get('syntara::config.uri') }}.'/logviewer')}}">
         <i class="glyphicon glyphicon-list-alt"></i><span>Logs</span>
     </a>
 </li>
